55be03ecb62f04ffac29de31b48bf165651ec658
agent87 <arnauldkayonga1@gmail.com>
2019-07-24 22:20:02 +0200

    Update g.py

--- g.py
+++ g.py
@@ -1,7 +1,7 @@
 #Python is High End language built oer c
 
 
-import RPi.GPIO as GPIO    #Importing Raspberry Pi GPIO pins controller module
+import RPi.GPIO as GPIO    #Importing Raspberry Pi GPIO pins controll module
 import time                #Importing timer module
 
 
@@ -11,40 +11,45 @@ GPIO.setwarnings(False)    # Turn off all warnings of sf.
 
 #Assigning the sensor a respective pin number
 #Open Terminal in Rasperry Pi; Type Pinout to inform yourself on the different pins.
-moisture = 7               #the moisture pin is given 7
-rain = 10                  #the rain sensor is given 10
+moisture_sensor = 7               #the moisture pin is given 7
+rain_sensor = 10                  #the rain sensor is given 10
 
-#Assigning the Power Relay Module respective pin number
-relay_1 = 5                
-relay_2 = 3                
+                           #Assigning the Power Relay Module respective pin number
+water_pump = 5                
+                
 
 
-GPIO.setup(moisture, GPIO.IN)      #
-GPIO.setup(rain, GPIO.IN)          #
-GPIO.setup(relay_1, GPIO.OUT)      #
-GPIO.setup(relay_2, GPIO.OUT)      #
-Raining = False                    # Set initial data as false
-
-GPIO.output(relay_1, False)        #
-GPIO.output(relay_2, False)        #
+GPIO.setup(moisture_sensor, GPIO.IN)      #
+GPIO.setup(rain_sensor, GPIO.IN)          #
+GPIO.setup(water_pump, GPIO.OUT)      #      #
 
 
 
-def irrigating_pump_actuator(moisture):                           #
-    if GPIO.input(moisture) == 1:                                 #
+Raining_Status = False                    # Set initial data as false
+
+GPIO.output(water_pump, False)        #
+        #
+
+
+
+def pump_actuator(moisture_sensor):                           #
+    if GPIO.input(moisture_sensor) == 1:                                 #
         print('Soil is Dry!')
-        if GPIO.input(rain) == 0:
+        if GPIO.input(rain_sensor) == 0:
             print("it is raining tho!, so i won't pump water")
-        elif GPIO.input(rain) == 1:
+        elif GPIO.input(rain_sensor) == 1:
             print('Soil is dry but not raining')
-            GPIO.output(relay_1, True)
-    elif GPIO.input(moisture) == 0:
+            GPIO.output(water_pump, True)
+    elif GPIO.input(moisture_sensor) == 0:
         print('---------------------------')
         print('Enough Water is in the soil')
         print('---------------------------')
-        GPIO.output(relay_1, False)
+        GPIO.output(water_pump, False)
         
 
 
-GPIO.add_event_detect(moisture, GPIO.BOTH, bouncetime=100)
-GPIO.add_event_callback(moisture, callback=irrigating_pump_actuator)
+GPIO.add_event_detect(moisture_sensor, GPIO.BOTH, bouncetime=100)
+GPIO.add_event_callback(moisture_sensor, callback=pump_actuator)
+
+
+    
